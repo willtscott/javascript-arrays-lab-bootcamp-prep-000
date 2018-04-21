580102398938a701ac63ebe0c4e405aea2104cab
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 00:50:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+ kittens.push(name)}  
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)}
+function destructivelyRemoveLastKitten(){
+ kittens.pop()}
+function destructivelyRemoveFirstKitten(){
+ kittens.shift()}
+function appendKitten(name){
+ return […kittens, name]}
+function prependKitten(name){
+ return [name, …kittens]}
