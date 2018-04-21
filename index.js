1b54277daeee2a97eab4f105171a8619fdd9717b
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 01:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function appendKitten(name){
 function prependKitten(name){
  return [name, ...kittens]}
 function removeLastKitten(){
- return kittens.slice(0, kittens.length-2)}
+ return kittens.slice(0, kittens.length-1)}
 
